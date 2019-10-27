ff6fc446453e770e4955299ba956cafc197030fa
Alex <aleksas@darkstar.lt>
2019-10-27 20:47:53 +0000

    Made tutorial and corrected canvas sizes

--- Scripts/ButtonController.cs
+++ Scripts/ButtonController.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class ButtonController : MonoBehaviour
 {
     public GameObject button;
+    public GameObject tutorial;
     public static bool startScore = false;
 
     public void HideButton()
     {
+        tutorial.SetActive(true);
         startScore = true;
         button.SetActive(false);
     }
